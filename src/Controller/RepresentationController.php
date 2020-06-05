96922328f8ef07958e7c8475cb9ed4b72bf1fe74
geoffrey gheysen <geoffrey.gheysen@hotmail.com>
2020-06-05 17:00:46 +0200

    controller Representation + templates + routes

---
+++ src/Controller/RepresentationController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Representation;
+
+class RepresentationController extends AbstractController
+{
+    /**
+     * @Route("/representation", name="representation")
+     */
+    public function index()
+    {
+        $repository = $this->getDoctrine()->getRepository(Representation::class);
+        $representations = $repository->findAll();
+
+        return $this->render('representation/index.html.twig', [
+            'representations' => $representations,
+            'ressource' => 'représentations',
+        ]);
+    }
+    /**
+     * @Route("/representation/{id}", name="representation_show")
+     */
+    public function show($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Representation::class);
+        $representation = $repository->find($id);
+
+        return $this->render('representation/show.html.twig', [
+            'representation' => $representation,
+        ]);
+    }
+}
